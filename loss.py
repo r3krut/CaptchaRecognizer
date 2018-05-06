b86202244aa3e1d4e8e55aa06f1aa7937372abe4
r3krut <tananakin.mikhail@gmail.com>
2018-05-06 20:36:36 +0300

    Loss function for multiclass segmentation was added

---
+++ loss.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import utils
+import numpy as np
+
+"""
+===Loss function for multiclass segmentation
+===Implementation from https://github.com/ternaus/robot-surgery-segmentation/loss.py
+"""
+
+class LossMulti:
+    def __init__(self, jaccard_weight=0, class_weights=None, num_classes=1):
+        if class_weights is not None:
+            nll_weight = utils.cuda(
+                torch.from_numpy(class_weights.astype(np.float32)))
+        else:
+            nll_weight = None
+        self.nll_loss = nn.NLLLoss2d(weight=nll_weight)
+        self.jaccard_weight = jaccard_weight
+        self.num_classes=num_classes
+
+    def __call__(self, outputs, targets):
+        loss = self.nll_loss(outputs, targets)
+        if self.jaccard_weight:
+            cls_weight = self.jaccard_weight / self.num_classes
+            eps = 1e-15
+            for cls in range(self.num_classes):
+                jaccard_target = (targets == cls).float()
+                jaccard_output = outputs[:, cls].exp()
+                intersection = (jaccard_output * jaccard_target).sum()
+
+                union = jaccard_output.sum() + jaccard_target.sum() + eps
+                loss += (1 - intersection / (union - intersection)) * cls_weight
+
+            loss /= (1 + self.jaccard_weight)
+        return loss
