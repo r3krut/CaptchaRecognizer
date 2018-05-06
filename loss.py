7ccf9e3d3c2f3dd3712da9a31086c364564d9fef
r3krut <tananakin.mikhail@gmail.com>
2018-05-06 20:39:45 +0300

    Fixes

--- loss.py
+++ loss.py
@@ -7,6 +7,7 @@ import numpy as np
 """
 ===Loss function for multiclass segmentation
 ===Implementation from https://github.com/ternaus/robot-surgery-segmentation/loss.py
+Vladimir Iglovikov
 """
 
 class LossMulti:
