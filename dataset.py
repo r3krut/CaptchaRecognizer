c604660aace5e785e1fef68c400a7afb3d9a142f
r3krut <tananakin.mikhail@gmail.com>
2018-05-09 13:52:11 +0300

    Some changes were done

--- dataset.py
+++ dataset.py
@@ -10,40 +10,47 @@ from torch.utils.data import Dataset
 from pathlib import Path
 import prepare_data
 
-data_path = Path('/home/r3krut/DataSets/NalogCaptchaDataTraining/CaptchaRecognition/train')
-
 class CaptchaDataset(Dataset):
-    def __init__(self, file_names: list, to_augment=False, transform=None, mode='train'):
+    def __init__(self, file_names: list, to_augment=False, transform=None, mode='train', img_channels=3):
         self.file_names = file_names
         self.to_augment = to_augment
         self.transform = transform
         self.mode = mode
+        self.img_channels = img_channels
 
     def __len__(self):
         return len(self.file_names)
 
     def __getitem__(self, idx):
         img_file_name = self.file_names[idx]
-        img = load_image(img_file_name)
-        mask = load_mask(img_file_name)
-
-        img, mask = self.transform(img, mask)
-
+        
         if self.mode == 'train':
-            return to_float_tensor(img), torch.from_numpy(mask).long()
+            img = load_image(img_file_name, img_channels=self.img_channels)
+            mask = load_mask(img_file_name)
+            img, mask = self.transform(img, mask)
+            return to_float_tensor(img, img_channels=self.img_channels), torch.from_numpy(mask).long()
         else:
-            return to_float_tensor(img), str(img_file_name)
+            img = load_image(img_file_name, img_channels=self.img_channels)
+            img = img[2:img.shape[0]-2, 4:img.shape[1]-4] #to (192,96)
+            return to_float_tensor(img, img_channels=self.img_channels), str(img_file_name)
+
+def expand(img):
+    return np.expand_dims(img, axis=0)
+
+def to_float_tensor(img, img_channels=1):
+    if img_channels == 1:
+        return torch.from_numpy(img).float()
+    else:    
+        return torch.from_numpy(np.moveaxis(img, -1, 0)).float()
 
 
-def to_float_tensor(img):
-    return torch.from_numpy(np.moveaxis(img, -1, 0)).float()
-
-
-def load_image(path):
-    img = cv2.imread(str(path))
-    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
+def load_image(path, img_channels=1):
+    img = cv2.imread(str(path), 0 if img_channels == 1 else 1)
+    if img_channels == 1:
+        return img
+    else:
+        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
 def load_mask(path):
     factor = prepare_data.digit_factor
     mask_folder = 'masks'
